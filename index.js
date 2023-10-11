182b9fa8165d9b847c65779bdbe1af799d915804
Vishal Ojha <74894133+VishalOjhaa@users.noreply.github.com>
2023-10-12 03:39:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+inquirer
+    .prompt([
+        {
+            message: "Type in your URL:",
+            name: "URL",
+        },
+    ])
+    .then((answers) => {
+        const url = answers.URL;
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('your-qr.png'));
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.error("Prompt couldn't be rendered in the current environment.");
+        } else {
+            console.error("An error occurred:", error);
+        }
+    });
